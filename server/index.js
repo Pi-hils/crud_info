410f6b5bded3f0f2d410e7be80e2334a0130ca08
Hilda Amponsah <hilda.amp@hotmail.com>
2021-02-16 21:32:15 +0000

    creating, viewing some/all data

--- server/index.js
+++ server/index.js
@@ -9,25 +9,44 @@ app.use(express.json());  //req.body
 
 //ROUTES
 
-//CREATE DETAILS
+//CREATE INFO
 app.post("/info", async (req, res) => {
   try {
     const { name } = req.body;
     const { email } = req.body;
     const newInfo = await pool.query(
-      "INSERT INTO info(name, email) VALUES($1, $2) RETURNING *",
-      [req.body.name, req.body.email]
+      "INSERT INTO info (name, email) VALUES('Agnes B','agnes@hello.com ) RETURNING *",
+       [name, email]
     );
-    res.json(newInfo)
+    res.json(newInfo,row[0])
   } catch (err) {
     console.error(err.message);
   }
 })
 
 
-//GET DETAILS
+//GET ALL INFO
+app.get("/info/:id", async(req,res) => {
+  try {
+    const allInfo = await pool.query("SELECT * FROM info");
+    res.json(allInfo.rows)
+  } catch (error) {
+    console.error(err.message)
+  }
+})
 
-//DELETE DETAILS
+// GET AN INNFO
+app.get("/todos/:id", async(req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("select * FROM innfo where info_id = $1",[id])
+    res.json(info.rows[0])
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+//DELETE INFO
 
 app.listen(3001, () => {
   console.log("Server has started on port 3000")
